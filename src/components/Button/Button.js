4641f8d3a6f94d118234efb9acc174999aab6850
DariiaNikolaieva <nikolaieva.dariya@gmail.com>
2021-07-30 13:16:44 +0300

    update

---
+++ src/components/Button/Button.js
@@ -0,0 +1,16 @@
+import PropTypes from "prop-types";
+import styles from "./Button.module.css";
+
+const Button = ({ loadMore }) => {
+  return (
+    <button onClick={loadMore} className={styles.Button}>
+      Load more
+    </button>
+  );
+};
+
+export default Button;
+
+Button.propTypes = {
+  loadMore: PropTypes.func,
+};
